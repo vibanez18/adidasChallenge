6f715fad2406808f5877dbb110bc19acfb2839b7
Victor <vibaneza@everis.com>
2018-06-09 20:56:22 +0200

    add data feign client

---
+++ core-api/src/main/java/com/adidas/challenge/api/data/TravelDto.java
@@ -0,0 +1,42 @@
+package com.adidas.challenge.api.data;
+
+import java.time.LocalDateTime;
+
+public class TravelDto {
+	private Long id;
+	private String city;
+	private String destinyCity;
+	private LocalDateTime departureTime;
+	private LocalDateTime arrivalTime;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getDestinyCity() {
+		return destinyCity;
+	}
+	public void setDestinyCity(String destinyCity) {
+		this.destinyCity = destinyCity;
+	}
+	public LocalDateTime getDepartureTime() {
+		return departureTime;
+	}
+	public void setDepartureTime(LocalDateTime departureTime) {
+		this.departureTime = departureTime;
+	}
+	public LocalDateTime getArrivalTime() {
+		return arrivalTime;
+	}
+	public void setArrivalTime(LocalDateTime arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+}
--- data-service/src/main/java/com/adidas/challenge/DataServiceApp.java
+++ data-service/src/main/java/com/adidas/challenge/DataServiceApp.java
@@ -38,9 +38,16 @@ public class DataServiceApp {
 	public void init() {
 		List<Travel> list = new ArrayList<>();
 		
-		list.add(new Travel("Valencia", "Madrid", LocalDateTime.of(2018, Month.JUNE, 18, 18, 0), LocalDateTime.of(2018, Month.JUNE, 18, 19, 45)));
-		list.add(new Travel("Valencia", "Barcelona", LocalDateTime.of(2018, Month.JUNE, 18, 18, 0), LocalDateTime.of(2018, Month.JUNE, 18, 21, 30)));
-		list.add(new Travel("Valencia", "Zaragoza", LocalDateTime.of(2018, Month.JUNE, 18, 17, 0), LocalDateTime.of(2018, Month.JUNE, 18, 19, 15)));
+		list.add(new Travel("Valencia", "Madrid", LocalDateTime.of(2018, Month.JUNE, 18, 10, 0), LocalDateTime.of(2018, Month.JUNE, 18, 11, 45)));
+		list.add(new Travel("Valencia", "Castellon", LocalDateTime.of(2018, Month.JUNE, 18, 10, 0), LocalDateTime.of(2018, Month.JUNE, 18, 11, 45)));
+		list.add(new Travel("Valencia", "Alicante", LocalDateTime.of(2018, Month.JUNE, 18, 10, 0), LocalDateTime.of(2018, Month.JUNE, 18, 11, 45)));
+		list.add(new Travel("Zaragoza", "Madrid", LocalDateTime.of(2018, Month.JUNE, 18, 11, 0), LocalDateTime.of(2018, Month.JUNE, 18, 13, 15)));
+		list.add(new Travel("Zaragoza", "Pamplona", LocalDateTime.of(2018, Month.JUNE, 18, 15, 0), LocalDateTime.of(2018, Month.JUNE, 18, 16, 30)));
+		list.add(new Travel("Zaragoza", "Bilbao", LocalDateTime.of(2018, Month.JUNE, 18, 14, 0), LocalDateTime.of(2018, Month.JUNE, 18, 15, 30)));
+		list.add(new Travel("Madrid", "Sevilla", LocalDateTime.of(2018, Month.JUNE, 18, 15, 0), LocalDateTime.of(2018, Month.JUNE, 18, 16, 30)));
+		list.add(new Travel("Madrid", "Malaga", LocalDateTime.of(2018, Month.JUNE, 18, 14, 0), LocalDateTime.of(2018, Month.JUNE, 18, 15, 30)));
+		list.add(new Travel("Madrid", "Valencia", LocalDateTime.of(2018, Month.JUNE, 18, 14, 0), LocalDateTime.of(2018, Month.JUNE, 18, 15, 30)));
+		list.add(new Travel("Madrid", "Zaragoza", LocalDateTime.of(2018, Month.JUNE, 18, 14, 0), LocalDateTime.of(2018, Month.JUNE, 18, 15, 30)));
 		
 		travelRepository.saveAll(list);
 	}
