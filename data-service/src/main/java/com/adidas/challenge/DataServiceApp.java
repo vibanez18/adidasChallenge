3ceca44622ee57094b5455f48f24e7c28057bb8b
Victor <vibaneza@everis.com>
2018-06-10 11:43:01 +0200

    Rest Service data-service

--- data-service/src/main/java/com/adidas/challenge/DataServiceApp.java
+++ data-service/src/main/java/com/adidas/challenge/DataServiceApp.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adidas.challenge.domain.Travel;
@@ -24,12 +23,6 @@ public class DataServiceApp {
 	
 	@Autowired TravelRepository travelRepository;
 	
-	//TODO: Remove it
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DataServiceApp.class, args);
 	}
